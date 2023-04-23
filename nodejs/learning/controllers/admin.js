702fcaf02adf0cd98bb5ede69c8016a95ee08de1
sadeq <erffon@gmail.com>
2023-04-23 22:33:52 +0330

    some adjustments

--- nodejs/learning/controllers/admin.js
+++ nodejs/learning/controllers/admin.js
@@ -1,3 +1,6 @@
 exports.getAbout = (req, res, next) => {
   res.render("about", { title: "About" });
 };
+exports.getProducts = (req, res) => {
+  res.render("products", { title: "Products Page" });
+};
--- nodejs/learning/routes/admin.js
+++ nodejs/learning/routes/admin.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const aboutController = require("../controllers/admin");
+const Controller = require("../controllers/admin");
 
-router.get("/about", aboutController.getAbout);
+router.get("/about", Controller.getAbout);
+router.get("/products", Controller.getProducts);
 router.post("/product", (req, res, next) => {
   console.log(req.body);
   res.render("index.js");
 });
 
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "home" });
 });
 
 module.exports = router;

4c68b7ff9b834d565010147722024323826bb33f
erfan <erffon@gmail.com>
2023-07-01 15:12:12 +0330

    first tests with jest for a simple js app

---
+++ testing/functions.test.js
@@ -0,0 +1,9 @@
+const functions = require("./functions");
+
+test("add function 2+2=4", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+test("should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});

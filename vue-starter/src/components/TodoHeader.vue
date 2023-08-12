4c162d86d0242ec060b41654849d45501e217373
erffon <erffon@gmail.com>
2023-08-12 13:58:03 +0330

    some html for nav section

--- vue-starter/src/components/TodoHeader.vue
+++ vue-starter/src/components/TodoHeader.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>first Vue app</div>
+  <nav class="container">
+    <div class="branding">
+      <img
+        src="../assets/logo.svg"
+        alt="logo"
+        style="width: 100px; height: 100px"
+      />
+      <h2>vue todo app</h2>
+      <ul class="nav-routes">
+        <RouterLink></RouterLink>
+      </ul>
+    </div>
+  </nav>
 </template>
 <style lang="scss" scoped></style>
 <scripts setup></scripts>
